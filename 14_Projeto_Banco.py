0914991dd485b7434f7e304d4e6f90034080bccf
claudiadjdantas <claudiadejesusdantas@gmail.com>
2025-12-15 16:24:06 -0300

    Adding the second part of the challenge: registering a user and account.

--- 14_Projeto_Banco.py
+++ 14_Projeto_Banco.py
@@ -1,5 +1,44 @@
 import textwrap
 
+def criar_usuario(usuarios):
+    cpf = input("Informe o seu CPF (somente números): ")
+    usuario = filtrar_usuario(cpf, usuarios)
+
+    if usuario:
+        print("\n@@@ Já existe um usuário com esse CPF. @@@")
+        return
+    
+    nome = input("Informe o nome completo: ")
+    data_nascimento = input("Informe a data de nascimento (dd-mm-aaaa): ")
+    endereco = input("Informe o endereço (logradouro, número, bairro, cidade, sigla estado): ")
+
+    usuarios.append({"nome": nome, "data_nascimento": data_nascimento, "cpf": cpf, "endereco": endereco})
+    
+    print("########### Usuário criado com sucesso! ###########")
+
+def filtrar_usuario(cpf, usuarios):
+    usuarios_filtrados = [usuario for usuario in usuarios if usuario["cpf"] == cpf]
+    return usuarios_filtrados[0] if usuarios_filtrados else None
+
+def criar_conta(agencia, numero_conta, usuarios):
+    cpf = input("Informe o CPF do usuário: ")
+    usuario = filtrar_usuario(cpf, usuarios)
+
+    if usuario:
+        print("\n#### Conta criada com sucesso ####")
+        return {"agencia": agencia, "numero_conta":numero_conta,"usuario":usuario}
+    print("\n@@@@ Usuário não encontrado, fluxo de criação de conta encerrado. @@@@")
+
+def listar_contas(contas):
+    for conta in contas:
+        linha = f"""
+            Agência:\t{conta['agencia']}
+            C/C:\t\t{conta['numero_conta']}
+            Titular:\t{conta['usuario']['nome']}
+        """
+        print("=" * 100)
+        print(textwrap.dedent(linha))
+
 # / => positional only
 def depositar(saldo, valor, extrato, /):
     if valor > 0:
@@ -46,6 +85,9 @@ def exibir_extrato(saldo, /,*,extrato):
 def menu(): 
     menu = """\n
     ======== MENU ======== \n
+    [nu]\t Criar Usuário
+    [nc]\t Criar Conta
+    [lc]\t Listar Contas
     [d]\t Depositar
     [s]\t Sacar
     [e]\t Extrato
@@ -55,14 +97,16 @@ def menu():
 
 def main():
     LIMITE_SAQUES = 3
-    # AGENCIA = "0001"
+    AGENCIA = "0001"
 
     saldo = 0
     limite = 500
     extrato = ""
     numero_saques = 0
-    # usuarios = []
-    # contas = []
+    usuarios = []
+    #o programa deve armazenar contas numa lista
+    contas = []
+    numero_conta = 1
 
     while True:
         opcao = menu()
@@ -85,6 +129,21 @@ def main():
 
         elif opcao == "e":
             exibir_extrato(saldo, extrato=extrato)
+
+        elif opcao == "nu":
+            criar_usuario(usuarios)
+
+        elif opcao == "nc":
+            # numero_conta = len(contas) + 1
+            conta = criar_conta(AGENCIA, numero_conta, usuarios)
+
+            if conta:
+                contas.append(conta)
+                #conta iniciando em 1
+                numero_conta += 1
+        
+        elif opcao == "lc":
+            listar_contas(contas)
         
         elif opcao == "q":
             break
